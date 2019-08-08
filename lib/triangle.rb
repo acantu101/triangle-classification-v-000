6424943b38ec94ba6f30823d3b2916e4c0b22232
alejandra cantu <alejandra.cantu13@gmail.com>
2019-08-08 22:26:56 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,28 @@
 class Triangle
   # write code here
+
+  attr_accessor :name
+
+def initialize(name)
+  @name = name
+end
+
+  def kind(type)
+  if type != :equilateral || type != :isosceles || type != :scalene
+    begin
+      raise TriangleError
+    rescue TriangleError => error
+      puts error.message
+    end
+  else 
+    return type
+  end
+
+
+class TriangleError < StandardError
+    def message
+      "you must give the kind method a a valid type argument"
+    end
+  end
+
 end
