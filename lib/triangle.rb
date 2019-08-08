2c1bd97cf3a1777ecf145f35d9b60da5cb7963b6
alejandra cantu <alejandra.cantu13@gmail.com>
2019-08-08 22:37:40 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,6 +10,12 @@ def initialize(a, b, c)
   if @a == @b && @b == @c
     @kind = :equilateral
     return @kind
+  elsif @a != @b && @b = @C || @a = @b && @b != @c || @a = @c && @b != @case 
+ @kind = :isosceles
+ return @kind
+    
+  end
+    
     end
   end
 end
