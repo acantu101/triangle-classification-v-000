e2d3609a2506f611e401ba14a170431d113b45e7
alejandra cantu <alejandra.cantu13@gmail.com>
2019-08-08 22:32:10 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,22 +13,4 @@ def initialize(a, b, c)
 end
 end
 
-  def kind(type)
-  if type != :equilateral || type != :isosceles || type != :scalene
-    begin
-      raise TriangleError
-    rescue TriangleError => error
-      puts error.message
-    end
-  else
-    return type
-  end
-end
-
-class TriangleError < StandardError
-    def message
-      "you must give the kind method a a valid type argument"
-    end
-  end
-
-end
+  
